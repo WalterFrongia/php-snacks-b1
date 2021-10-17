56cb17a35ae50bdf451e065bdf89b1ff87e60b6a
WalterFrongia <walter.frongia@libero.it>
2021-10-17 16:56:50 +0200

    Impostato ordine in index snack repository

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <!-- SNAK 1 -->
+<h1>SNACK 1</h1>
 <!-- 
     Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario, di 8 partite. 
     Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
     Stampiamo a schermo tutte le partite con questo schema.
 -->
-
+<hr>
 
 <!-- SNAK 2 -->
+<h1>SNACK 2</h1>
 <!-- 
     Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri,
     che mail contenga un punto e una chiocciola e che age sia un numero.
     Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato" 
 -->
+<hr>
 
 
 <!-- SNAK 3 -->
-
+<h1>SNACK 3</h1>
 <!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array
     di post associati a quella data. Stampare ogni data con i relativi post.
     Qui l'array di esempio: https://www.codepile.net/pile/R2K5d68z 
@@ -68,27 +71,20 @@ foreach($posts as $chiave => $articles) {
     <?php }?>
 <?php } ?>
 
-
+<hr>
 
 <!-- SNAK 4 -->
-
+<h1>SNACK 4</h1>
 <!-- Creare un array con 15 numeri casuali, 
     tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta 
 -->
-
-<?php
-    $randomNumbers = [];
-
-?>
-
+<hr>
 
 <!-- SNAK 5 -->
-
+<h1>SNACK 5</h1>
 <!-- Utilizzare questo array: https://pastebin.com/CkX3680A
     Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. 
--->
-
-<h1>SNACK 5</h1> 
+--> 
 
 <?php
     $db = [
@@ -137,9 +133,11 @@ foreach($posts as $chiave => $articles) {
     
 </div>
 
+<hr>
+
 
 <!-- SNAK 6 -->
-
+<h1>SNACK 6</h1>
 <!-- Creare un array contenente qualche alunno di un'ipotetica classe. 
 Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
 Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
@@ -182,8 +180,10 @@ Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
 
 <?php } ?>
 
-<!-- SNAK 7 -->
+<hr>
 
+
+<!-- SNAK 7 -->
 <!--  
 Prendere un paragrafo abbastanza lungo, contenente diverse frasi. PrenderSe il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
 -->
